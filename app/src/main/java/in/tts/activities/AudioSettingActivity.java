04e9f31e2d4cc8b5bcf96b004875e87e1342e2b0
dineshdtt <37068519+dineshdtt@users.noreply.github.com>
2018-06-27 13:55:53 +0530

    register fragnment
    create

---
+++ app/src/main/java/in/tts/activities/AudioSettingActivity.java
@@ -0,0 +1,16 @@
+package in.tts.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import in.tts.R;
+
+public class AudioSettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_setting);
+    }
+
+}
