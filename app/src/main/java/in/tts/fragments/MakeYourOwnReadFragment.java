ab472592a8e43b484a791323610f2fc93c0cac82
dineshdtt <37068519+dineshdtt@users.noreply.github.com>
2018-06-29 17:58:01 +0530

    make your read
    comiit

--- app/src/main/java/in/tts/fragments/MakeYourOwnReadFragment.java
+++ app/src/main/java/in/tts/fragments/MakeYourOwnReadFragment.java
@@ -2,14 +2,18 @@ package in.tts.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import in.tts.R;
 
 public class MakeYourOwnReadFragment extends Fragment {
+
+    private EditText editText;
 
     public MakeYourOwnReadFragment() {
         // Required empty public constructor
@@ -20,6 +24,18 @@ public class MakeYourOwnReadFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_make_your_own_read, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        editText = getActivity().findViewById(R.id.edMakeRead);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setBackgroundColor(getResources().getColor(R.color.transparent));
+            }
+        });
     }
 }
 
