295dfd35ae51d1e5a07452ff264397d9d97d4995
akshipta@dttminer.com <37068505+Akshipta@users.noreply.github.com>
2018-06-29 13:38:51 +0530

    tutorial commit , Login regiastration, home amin activity

--- app/src/main/java/in/tts/fragments/RegisterFragment.java
+++ app/src/main/java/in/tts/fragments/RegisterFragment.java
@@ -1,13 +1,17 @@
 package in.tts.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import in.tts.R;
+import in.tts.activities.LoginActivity;
+import in.tts.activities.MainActivity;
 
 public class RegisterFragment extends Fragment {
 
@@ -24,4 +28,30 @@ public class RegisterFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        try {
+            getActivity().findViewById(R.id.txtAlreadyAccountReg).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        startActivity(new Intent(getContext(), LoginActivity.class).putExtra("LOGIN", "login"));
+                        getActivity().finish();
+                    } catch (Exception | Error e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            getActivity().findViewById(R.id.txtSkipRegisterReg).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getContext(), MainActivity.class));
+                    getActivity().finish();
+                }
+            });
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+        }
+    }
 }
