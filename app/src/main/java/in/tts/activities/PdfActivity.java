725ac2a4dac5d93f29e21a8def719afd0dd4dd16
akshipta@dttminer.com <37068505+Akshipta@users.noreply.github.com>
2018-06-29 18:31:26 +0530

    final send

--- app/src/main/java/in/tts/activities/PdfActivity.java
+++ app/src/main/java/in/tts/activities/PdfActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
@@ -24,70 +25,100 @@ public class PdfActivity extends AppCompatActivity implements OnPageChangeListen
     PDFView pdfView;
     Integer pageNumber = 0;
     String pdfFileName;
-    String TAG ="PdfActivity";
+    String TAG = "PdfActivity";
     int position = -1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdf);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().show();
-            getSupportActionBar().setTitle(R.string.app_name);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setDisplayShowTitleEnabled(true);
-            getSupportActionBar().setHomeAsUpIndicator(ContextCompat.getDrawable(this, R.drawable.ic_backward));
-        } else {
-            getSupportActionBar().setTitle(R.string.app_name);
-        }
+        try {
+            setContentView(R.layout.activity_pdf);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().show();
+                getSupportActionBar().setTitle(R.string.app_name);
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+                getSupportActionBar().setDisplayShowTitleEnabled(true);
+                getSupportActionBar().setHomeAsUpIndicator(ContextCompat.getDrawable(this, R.drawable.ic_backward));
+            } else {
+                getSupportActionBar().setTitle(R.string.app_name);
+            }
 
-        init();
+            init();
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void init() {
-        pdfView = (PDFView) findViewById(R.id.pdfView);
-        position = getIntent().getIntExtra("position", -1);
-        displayFromSdcard();
+        try {
+            pdfView = (PDFView) findViewById(R.id.pdfView);
+            position = getIntent().getIntExtra("position", -1);
+            displayFromSdcard();
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void displayFromSdcard() {
-        pdfFileName = DocumentsFragment.fileList.get(position).getName();
+        try {
+            pdfFileName = DocumentsFragment.fileList.get(position).getName();
 
-        pdfView.fromFile(DocumentsFragment.fileList.get(position))
-                .defaultPage(pageNumber)
-                .enableSwipe(true)
+            pdfView.fromFile(DocumentsFragment.fileList.get(position))
+                    .defaultPage(pageNumber)
+                    .enableSwipe(true)
 
-                .swipeHorizontal(false)
-                .onPageChange(this)
-                .enableAnnotationRendering(true)
-                .onLoad(this)
-                .scrollHandle(new DefaultScrollHandle(this))
-                .load();
+                    .swipeHorizontal(false)
+                    .onPageChange(this)
+                    .enableAnnotationRendering(true)
+                    .onLoad(this)
+                    .scrollHandle(new DefaultScrollHandle(this))
+                    .load();
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onPageChanged(int page, int pageCount) {
-        pageNumber = page;
+        try {
+            pageNumber = page;
 //        setTitle(String.format(" % s % s / % s",pdfFileName, page + 1, pageCount));
-        setTitle(DocumentsFragment.fileList.get(position).getName());
+            setTitle(DocumentsFragment.fileList.get(position).getName());
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void loadComplete(int nbPages) {
-        PdfDocument.Meta meta = pdfView.getDocumentMeta();
-        printBookmarksTree(pdfView.getTableOfContents(), " -");
+        try {
+            PdfDocument.Meta meta = pdfView.getDocumentMeta();
+            printBookmarksTree(pdfView.getTableOfContents(), " -");
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
     public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
-        for (PdfDocument.Bookmark b : tree) {
+        try {
+            for (PdfDocument.Bookmark b : tree) {
 
-            Log.e(TAG, String.format(" % s % s, p % d", sep, b.getTitle(), b.getPageIdx()));
+                Log.e(TAG, String.format(" % s % s, p % d", sep, b.getTitle(), b.getPageIdx()));
 
-            if (b.hasChildren()) {
-                printBookmarksTree(b.getChildren(), sep + " -");
+                if (b.hasChildren()) {
+                    printBookmarksTree(b.getChildren(), sep + " -");
+                }
             }
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -107,7 +138,12 @@ public class PdfActivity extends AppCompatActivity implements OnPageChangeListen
     }
 
     public void onBackPressed() {
-        super.onBackPressed();
-        finish();
+        try {
+            super.onBackPressed();
+            finish();
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Toast.makeText(PdfActivity.this, " Error in Displaying Pdf ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/in/tts/services/FloatingViewService.java
@@ -0,0 +1,59 @@
+package in.tts.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import in.tts.R;
+
+public class FloatingViewService extends Service {
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Inflate the floating view layout we created
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+        //Add the view to the window.
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        //Specify the view position
+        params.gravity = Gravity.TOP | Gravity.LEFT;        //Initially view will be added to top-left corner
+        params.x = 0;
+        params.y = 100;
+
+        //Add the view to the window
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+        //….
+        //….
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+    }
+}
