1b8e979ca4f07065daa6609aebc7d9498e5925db
dineshdtt <37068519+dineshdtt@users.noreply.github.com>
2018-07-04 17:22:04 +0530

    recent audio  file
    commit

--- app/src/main/java/in/tts/adapters/RecentVoiceAdapter.java
+++ app/src/main/java/in/tts/adapters/RecentVoiceAdapter.java
@@ -1,43 +1,59 @@
 package in.tts.adapters;
 
 import android.content.Context;
+import android.graphics.Movie;
+import android.speech.tts.Voice;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 
+import java.util.List;
+
 import in.tts.R;
+import in.tts.model.AudioRecent;
 
 public class RecentVoiceAdapter extends RecyclerView.Adapter<RecentVoiceAdapter.ViewHolder> {
     private Context context;
+    private List<AudioRecent> AudioList;
 
-    public RecentVoiceAdapter(Context ctx) {
+    public RecentVoiceAdapter(Context ctx, List<AudioRecent> AudioList) {
         context = ctx;
+        this.AudioList = AudioList;
     }
 
 
     @NonNull
     @Override
     public RecentVoiceAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.recent_voice_item,viewGroup, false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.recent_voice_item, viewGroup, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecentVoiceAdapter.ViewHolder viewHolder, int i) {
 
+        AudioRecent audioRecent = AudioList.get(i);
+        viewHolder.Filename.setText(audioRecent.getFilename());
+        viewHolder.FileSize.setText(audioRecent.getFileSize());
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return AudioList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView Filename, FileSize;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            Filename = (TextView) itemView.findViewById(R.id.tv_file_name);
+            FileSize = (TextView) itemView.findViewById(R.id.tv_file_size);
         }
     }
 }
--- app/src/main/java/in/tts/fragments/MakeYourOwnReadFragment.java
+++ app/src/main/java/in/tts/fragments/MakeYourOwnReadFragment.java
@@ -1,15 +1,23 @@
 package in.tts.fragments;
 
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.perf.metrics.AddTrace;
+
+import java.util.Locale;
 
 import in.tts.R;
 import in.tts.utils.CommonMethod;
@@ -17,6 +25,8 @@ import in.tts.utils.CommonMethod;
 public class MakeYourOwnReadFragment extends Fragment {
 
     private EditText editText;
+    private TextToSpeech t1;
+    private Button b1;
 
     public MakeYourOwnReadFragment() {
         // Required empty public constructor
@@ -38,12 +48,43 @@ public class MakeYourOwnReadFragment extends Fragment {
         CommonMethod.setAnalyticsData(getContext(), "MainTab", "MakeYourRead", null);
 
         editText = getActivity().findViewById(R.id.edMakeRead);
+        b1=getActivity().findViewById(R.id.button);
         editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                editText.setBackgroundColor(getResources().getColor(R.color.transparent));
+                editText.setBackgroundColor(getResources().getColor(R.color.transparent));
+            }
+        });
+
+
+        t1=new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        final ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.addPrimaryClipChangedListener( new ClipboardManager.OnPrimaryClipChangedListener() {
+            public void onPrimaryClipChanged() {
+                String toSpeak = clipboard.getText().toString();
+//                String toSpeak = editText.getText().toString();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
             }
         });
     }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
 }
+
+
 
