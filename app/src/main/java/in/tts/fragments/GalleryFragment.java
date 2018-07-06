a1eebe132b1f26589864c2a735d0731750e2da3a
dineshdtt <37068519+dineshdtt@users.noreply.github.com>
2018-07-06 17:27:24 +0530

    Gallery chnges

--- app/src/main/java/in/tts/fragments/GalleryFragment.java
+++ app/src/main/java/in/tts/fragments/GalleryFragment.java
@@ -13,6 +13,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,8 +37,9 @@ import in.tts.activities.ImageOcrActivity;
 
 public class GalleryFragment extends Fragment {
 
-    private GridView gallery;
+    //    private GridView gallery;
     private ArrayList<String> images;
+    private RecyclerView recyclerView;
 
     public GalleryFragment() {
     }
@@ -51,7 +55,8 @@ public class GalleryFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        gallery = getActivity().findViewById(R.id.gridView);
+//        gallery = getActivity().findViewById(R.id.gridView);
+        recyclerView = getActivity().findViewById(R.id.rvGallery);
         per();
         toGetData();
     }
@@ -69,23 +74,60 @@ public class GalleryFragment extends Fragment {
 
     private void toGetData() {
         try {
-            gallery.setAdapter(new ImageAdapter(getActivity()));
-            gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-
-                    if (null != images && !images.isEmpty()) {
-                        startActivity(new Intent(getContext(), ImageOcrActivity.class).putExtra("PATH", images.get(position)));
-//                        Toast.makeText(getContext(), "position " + position + " " + images.get(position), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+//            gallery.setAdapter(new ImageAdapter(getActivity()));
+//            gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//                @Override
+//                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+//
+//                    if (null != images && !images.isEmpty()) {
+//                        startActivity(new Intent(getContext(), ImageOcrActivity.class).putExtra("PATH", images.get(position)));
+////                        Toast.makeText(getContext(), "position " + position + " " + images.get(position), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+            recyclerView.setAdapter(new ImageAdapterGallery(getActivity(), getAllShownImagesPath(getActivity())));
         } catch (Exception | Error e) {
             e.printStackTrace();
         }
     }
 
+    private ArrayList<String> getAllShownImagesPath(Activity activity) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        String absolutePathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            listOfAllImages.add(absolutePathOfImage);
+        }
+        Log.d("TAG", " DATa " + listOfAllImages.size()+":"+listOfAllImages);
+        return listOfAllImages;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                toGetData();
+            } else {
+                Toast.makeText(getContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
+
+/*
     private class ImageAdapter extends BaseAdapter {
 
         private Activity context;
@@ -96,6 +138,7 @@ public class GalleryFragment extends Fragment {
         }
 
         public int getCount() {
+            Log.d("TAG", " count images " + images.size());
             return images.size();
         }
 
@@ -119,46 +162,15 @@ public class GalleryFragment extends Fragment {
                 int width = displayMetrics.widthPixels;
                 int size = width / 3;
 
-                picturesView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, size - 10));
+                picturesView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, size-10));
 //                picturesView.setPadding(5, 5, 5, 5);
 
             } else {
                 picturesView = (ImageView) convertView;
             }
-            Log.d("TAG", "Images " + images.get(position) + " : " + position);
-            Picasso.get().load("file://" + images.get(position).replaceAll("\\s", "%20")).into(picturesView);
+//            Glide.with(context).load(images.get(position)).into(picturesView);
+            Log.d("TAG","Images " + images.get(position) + " : " + position);
+            Picasso.get().load("file://"+images.get(position).replaceAll("\\s", "%20")).into(picturesView);
             return picturesView;
         }
-
-        private ArrayList<String> getAllShownImagesPath(Activity activity) {
-            Uri uri;
-            Cursor cursor;
-            int column_index_data, column_index_folder_name;
-            ArrayList<String> listOfAllImages = new ArrayList<String>();
-            String absolutePathOfImage = null;
-            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-
-            String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
-            cursor = activity.getContentResolver().query(uri, projection, null, null, null);
-            column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-            column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-            while (cursor.moveToNext()) {
-                absolutePathOfImage = cursor.getString(column_index_data);
-                listOfAllImages.add(absolutePathOfImage);
-            }
-            return listOfAllImages;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 10) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                toGetData();
-            } else {
-                Toast.makeText(getContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-}
+* */
---
+++ app/src/main/java/in/tts/fragments/ImageAdapterGallery.java
@@ -0,0 +1,57 @@
+package in.tts.fragments;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.crashlytics.android.Crashlytics;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import in.tts.R;
+
+class ImageAdapterGallery extends RecyclerView.Adapter<ImageAdapterGallery.ViewHolder> {
+    private Context context;
+    private ArrayList<String> images;
+
+    public ImageAdapterGallery(Context activity, ArrayList<String> imagesGallery) {
+        context = activity;
+        images = imagesGallery;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.image_item, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        try {
+            Picasso.get().load("file://" + images.get(i).replaceAll("\\s", "%20")).into(viewHolder.picturesView);
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Crashlytics.logException(e);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView picturesView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            picturesView = itemView.findViewById(R.id.ivItem);
+        }
+    }
+}
