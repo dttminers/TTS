363fc5304be2b356d9a1bfb136183e61a5d22ef7
dineshdtt <37068519+dineshdtt@users.noreply.github.com>
2018-07-06 13:03:12 +0530

    only Google Login commit

--- app/src/main/java/in/tts/fragments/LoginFragment.java
+++ app/src/main/java/in/tts/fragments/LoginFragment.java
@@ -1,6 +1,5 @@
 package in.tts.fragments;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -20,6 +19,8 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
@@ -33,7 +34,13 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.perf.metrics.AddTrace;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.List;
 
 import in.tts.R;
 import in.tts.activities.LoginActivity;
@@ -48,6 +55,7 @@ public class LoginFragment extends Fragment {
     private RelativeLayout relativeLayoutGoogle, relativeLayoutFb;
     private CallbackManager callbackManager;
     private AccessTokenTracker accessTokenTracker;
+    //    private LoginButton loginButton;
     private AccessToken accessToken;
     private ProfileTracker profileTracker;
     private int RC_SIGN_IN = 123;
@@ -58,18 +66,19 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+    @Override
     @AddTrace(name = "onCreateLoginFragment", enabled = true)
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(this.getContext());
-        callbackManager = CallbackManager.Factory.create();
-
-        relativeLayoutGoogle = getActivity().findViewById(R.id.rlGoogleLogin);
-        relativeLayoutFb = getActivity().findViewById(R.id.rlFacebookLogin);
-
-        CommonMethod.setAnalyticsData(getContext(), "MainTab", "Login", null);
         try {
+            CommonMethod.setAnalyticsData(getContext(), "MainTab", "Login", null);
+
+            // View
             getActivity().findViewById(R.id.llSignUp).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -89,66 +98,34 @@ public class LoginFragment extends Fragment {
                     getActivity().finish();
                 }
             });
+
+            // Google
+            relativeLayoutGoogle = getActivity().findViewById(R.id.rlGoogleLogin);
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build();
+
+            mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+            relativeLayoutGoogle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                    startActivityForResult(signInIntent, RC_SIGN_IN);
+                }
+            });
+            // Google
+
         } catch (Exception | Error e) {
             e.printStackTrace();
             Crashlytics.logException(e);
         }
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
-        relativeLayoutGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-
-
-        accessTokenTracker = new AccessTokenTracker() {
-            @Override
-            protected void onCurrentAccessTokenChanged(
-                    AccessToken oldAccessToken,
-                    AccessToken currentAccessToken) {
-                // Set the access token using
-                // currentAccessToken when it's loaded or set.
-            }
-        };
-
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(
-                    Profile oldProfile,
-                    Profile currentProfile) {
-                // App code
-            }
-        };
-        // If the access token is available already assign it.
-        accessToken = AccessToken.getCurrentAccessToken();
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Google
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
@@ -158,13 +135,6 @@ public class LoginFragment extends Fragment {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             if (account != null) {
-                String personName = account.getDisplayName();
-                String personGivenName = account.getGivenName();
-                String personFamilyName = account.getFamilyName();
-                String personEmail = account.getEmail();
-                String personId = account.getId();
-                Uri personPhoto = account.getPhotoUrl();
-
                 Log.d("TAG", " Name : " + account.getDisplayName());
                 Log.d("TAG", " ID : " + account.getId());
                 User user = User.getUser(getContext());
@@ -178,27 +148,13 @@ public class LoginFragment extends Fragment {
 
                 toExit();
             }
-
-            // Signed in successfully, show authenticated UI.
-//            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-//            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-//            updateUI(null);
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            Crashlytics.logException(e);
         }
     }
 
-//    private void toSetUserData() {
-//        try{
-//
-//    } catch (Exception | Error e) {
-//        e.printStackTrace();
-//        Crashlytics.logException(e);
-//    }
-//    }
-
-
+    // Called when successfully logged in
     private void toExit() {
         try {
             PrefManager.setUserInfo(getContext());
@@ -208,17 +164,4 @@ public class LoginFragment extends Fragment {
             Crashlytics.logException(e);
         }
     }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        accessTokenTracker.stopTracking();
-        profileTracker.stopTracking();
-    }
-
 }
