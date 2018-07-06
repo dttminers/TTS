f2676a1e85fa028c70031a80a5656ab695497e88
akshipta@dttminer.com <37068505+Akshipta@users.noreply.github.com>
2018-07-06 18:45:22 +0530

    homePage

---
+++ app/src/main/java/in/tts/adapters/PDFHomePageImages.java
@@ -0,0 +1,57 @@
+package in.tts.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+
+import in.tts.R;
+
+public class PDFHomePageImages extends PagerAdapter {
+    ArrayList<String> l;
+    Context context;
+
+    public PDFHomePageImages(Context ctx, ArrayList<String> list) {
+        context = ctx;
+        l = list;
+    }
+
+    @Override
+    public int getCount() {
+        return l.size();
+    }
+
+
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public Object instantiateItem(ViewGroup container, int position) {
+        ViewGroup vg = null;
+        try {
+            vg = (ViewGroup) LayoutInflater.from(this.context).inflate(R.layout.image_item, container, false);
+            ImageView iv = vg.findViewById(R.id.ivItem);
+//            iv.setBackgroundColor(toGetRandomColor());
+            Picasso.get().load("file://" + l.get(position).replaceAll("\\s", "%20")).into(iv);
+            container.addView(vg);
+        }catch (Exception| Error e){
+            e.printStackTrace();
+        }
+        return vg;
+    }
+
+    public int toGetRandomColor() {
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(200), rnd.nextInt(200), rnd.nextInt(200));
+    }
+}
+
--- app/src/main/java/in/tts/fragments/HomePageFragment.java
+++ app/src/main/java/in/tts/fragments/HomePageFragment.java
@@ -1,23 +1,44 @@
 package in.tts.fragments;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.firebase.perf.metrics.AddTrace;
 
+import org.json.JSONArray;
+
+import java.io.File;
+import java.util.ArrayList;
+
 import in.tts.R;
+import in.tts.adapters.PDFAdapter;
+import in.tts.adapters.PDFHomePage;
+import in.tts.adapters.PDFHomePageImages;
 import in.tts.utils.CommonMethod;
 
 public class HomePageFragment extends Fragment {
@@ -26,8 +47,15 @@ public class HomePageFragment extends Fragment {
     ImageView imageView;
     ImageView ivLeft, ivRight;
     CustomPagerAdapter mCustomPagerAdapter;
+    LinearLayout ll;
     int currentImage = 0;
     int mResources[] = {R.drawable.t1, R.drawable.t2, R.drawable.t3, R.drawable.t4, R.drawable.t5};
+    boolean boolean_permission;
+    private File dir;
+
+    public static ArrayList<File> fileList = new ArrayList<File>();
+    public static int REQUEST_PERMISSIONS = 1;
+
 
     public HomePageFragment() {
         // Required empty public constructor
@@ -49,6 +77,9 @@ public class HomePageFragment extends Fragment {
             ivRight = getActivity().findViewById(R.id.imageViewRight);
             tabLayout = getActivity().findViewById(R.id.tlHomePage);
             mViewPager = getActivity().findViewById(R.id.vpHomePage);
+
+            ll = getActivity().findViewById(R.id.llData);
+
             mCustomPagerAdapter = new CustomPagerAdapter(mResources, getContext());
             mViewPager.setAdapter(mCustomPagerAdapter);
             tabLayout.setupWithViewPager(mViewPager);
@@ -83,7 +114,154 @@ public class HomePageFragment extends Fragment {
                 }
             }
         });
+
+        toSetData();
     }
+
+    private void toSetData() {
+        dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        fn_permission();
+
+
+
+    }
+
+    private void fn_permission() {
+
+        try {
+            Log.d("TAG", " pdf permission ");
+            if ((ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+                if ((ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.READ_EXTERNAL_STORAGE))) {
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS);
+                }
+            } else {
+                toGetPDF();
+            }
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void toGetPDF() {
+        try {
+            Log.d("TAG", " pdf getpdf ");
+            boolean_permission = true;
+            getfile(dir);
+            toBindDealProductData(fileList, "Recent PDF ", "See More");
+
+            toBindDealProductDataimages(getAllShownImagesPath(getActivity()), "Recent Images ", "See More");
+
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void toBindDealProductDataimages(ArrayList<String> fileList, String header, String see_more) {
+        try {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (inflater != null) {
+                View view = inflater.inflate(R.layout.layout_home_page_recent_items, null, false);
+                TextView tvHeader = (TextView) view.findViewById(R.id.tvRecent);
+                TextView tvHeaderTimer = (TextView) view.findViewById(R.id.tvSeeMore);
+                ViewPager vpDeals = (ViewPager) view.findViewById(R.id.vpRecentItem);
+                tvHeader.setText(header);
+                vpDeals.setClipToPadding(false);
+//                vpDeals.setPadding(15, 0, 15, 0);
+                vpDeals.setOffscreenPageLimit(4);
+                vpDeals.setPageMargin(10);
+                vpDeals.setAdapter(new PDFHomePageImages(getContext(), fileList));
+                this.ll.addView(view);
+            }
+        } catch (Exception|Error e){
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<String> getAllShownImagesPath(Activity activity) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        String absolutePathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            listOfAllImages.add(absolutePathOfImage);
+        }
+        Log.d("TAG", " DATa " + listOfAllImages.size()+":"+listOfAllImages);
+        return listOfAllImages;
+    }
+
+    @AddTrace(name = "onGetPDF", enabled = true)
+    public ArrayList<File> getfile(File dir) {
+        File listFile[] = dir.listFiles();
+        if (listFile != null && listFile.length > 0) {
+            for (int i = 0; i < listFile.length; i++) {
+                if (listFile[i].isDirectory()) {
+                    getfile(listFile[i]);
+                } else {
+                    boolean booleanpdf = false;
+                    if (listFile[i].getName().endsWith(".pdf")) {
+                        for (int j = 0; j < fileList.size(); j++) {
+                            if (fileList.get(j).getName().equals(listFile[i].getName())) {
+                                booleanpdf = true;
+                            } else {
+                            }
+                        }
+                        if (booleanpdf) {
+                            booleanpdf = false;
+                        } else {
+                            fileList.add(listFile[i]);
+                        }
+                    }
+                }
+            }
+        }
+        Log.d("TAG", " pdf count " + fileList.size());
+        return fileList;
+    }
+
+
+    private void toBindDealProductData( ArrayList<File> list,  String header, String viewAll) {
+        try {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (inflater != null) {
+                View view = inflater.inflate(R.layout.layout_home_page_recent_items, null, false);
+                TextView tvHeader = (TextView) view.findViewById(R.id.tvRecent);
+                TextView tvHeaderTimer = (TextView) view.findViewById(R.id.tvSeeMore);
+                ViewPager vpDeals = (ViewPager) view.findViewById(R.id.vpRecentItem);
+                tvHeader.setText(header);
+                vpDeals.setClipToPadding(false);
+//                vpDeals.setPadding(15, 0, 15, 0);
+                vpDeals.setOffscreenPageLimit(4);
+                vpDeals.setPageMargin(10);
+                vpDeals.setAdapter(new PDFHomePage(getContext(), list));
+                this.ll.addView(view);
+            }
+        } catch (Exception|Error e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                toGetPDF();
+            } else {
+                Toast.makeText(getContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 
     class CustomPagerAdapter extends PagerAdapter {
 
